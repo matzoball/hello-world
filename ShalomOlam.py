4777bff2d6c033ba50950a4759f56d0ebc2d0863
Jeffrey A "Jeff" Karas <75324897+matzoball@users.noreply.github.com>
2020-12-01 13:07:18 -0500

    Create ShalomOlam.py
    Hello World with a Hebrew-language twist.

---
+++ ShalomOlam.py
@@ -0,0 +1,7 @@
+name = "גרשון אברם בן הלל"
+company = "Itero Group, LLC"
+print(f"{name} works at {company}.")
+
+hello_in_Hebrew = "שלום"
+status_message = "Working From Home"
+print(f"{status_message} {hello_in_Hebrew}")
